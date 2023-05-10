cae8c1da0bb62efc76961d1787695625fdac5c05
Emmanuel <emmanuelutofa@gmail.com>
2023-05-10 23:16:49 +0200

    made changes

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
 import './styles.css';
 
-const tasks = [
-  {
-    description: 'Wash the dishes',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Complete the To Do list project',
-    completed: true,
-    index: 2,
-  },
-];
-
-function renderTodoList() {
-  const todoList = document.getElementById('todo-list');
-  tasks.sort((a, b) => a.index - b.index);
-  todoList.innerHTML = tasks
-    .map(
-      (task) => `<li class="todo-item ${task.completed ? 'completed' : ''}">
-          <input type="checkbox" ${task.completed ? 'checked' : ''}>
-          <span>${task.description}</span>
-          <hr class="todo-hr">
-        </li>`,
-    )
-    .join('');
-}
-
-renderTodoList();
+a
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  watch: true,
   module: {
     rules: [
       {
